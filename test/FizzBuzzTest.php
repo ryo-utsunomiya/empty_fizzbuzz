8e025f2c406aacef0046296fea94c5aa640ef379
Ryo Utsunomiya <utsunomiya@asial.co.jp>
2015-04-20 11:06:43 +0900

    init

---
+++ test/FizzBuzzTest.php
@@ -0,0 +1,12 @@
+<?php
+
+class FizzBuzzTest extends PHPUnit_Framework_TestCase
+{
+  /**
+   * @test
+   */
+  public function dummyTest()
+  {
+    $this->assertThat(true, $this->isTrue());
+  }
+}
