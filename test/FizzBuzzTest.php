f303d22ad9c16ca5e4c86ba27fe048097e1b6e4c
Ryo Utsunomiya <utsunomiya@asial.co.jp>
2015-04-20 11:17:21 +0900

    add require of FizzBuzz.php

--- test/FizzBuzzTest.php
+++ test/FizzBuzzTest.php
@@ -1,12 +1,14 @@
 <?php
 
+require_once __DIR__ . '/../FizzBuzz.php';
+
 class FizzBuzzTest extends PHPUnit_Framework_TestCase
 {
-  /**
-   * @test
-   */
-  public function dummyTest()
-  {
-    $this->assertThat(true, $this->isTrue());
-  }
+    /**
+    * @test
+    */
+    public function dummyTest()
+    {
+        $this->assertThat(true, $this->isTrue());
+    }
 }
