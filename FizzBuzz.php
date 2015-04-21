ff25fabf2f2756ec22c27ed6900cb8a896a89667
Ryo Utsunomiya <utsunomiya@asial.co.jp>
2015-04-21 11:31:08 +0900

    add answer example

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -2,4 +2,26 @@
 
 namespace Acme;
 
-class FizzBuzz {}
+class FizzBuzz
+{
+    /**
+     * @param $n
+     * @return string
+     */
+    public function fizzbuzz($n)
+    {
+        if (!ctype_digit((string)$n)) {
+            throw new \InvalidArgumentException('parameter must be natural number');
+        }
+        if ($n % 15 === 0) {
+            return "FizzBuzz";
+        }
+        if ($n % 5 === 0) {
+            return "Buzz";
+        }
+        if ($n % 3 === 0) {
+            return "Fizz";
+        }
+        return (string)$n;
+    }
+}
--- test/FizzBuzzTest.php
+++ test/FizzBuzzTest.php
@@ -5,10 +5,50 @@ require_once __DIR__ . '/../FizzBuzz.php';
 class FizzBuzzTest extends PHPUnit_Framework_TestCase
 {
     /**
-    * @test
-    */
+     * @test
+     */
     public function dummyTest()
     {
         $this->assertThat(true, $this->isTrue());
+    }
+
+    /**
+     * @test
+     */
+    public function fizzbuzzに15の倍数を渡すとFizzBuzzを返す()
+    {
+        $fz = new Acme\FizzBuzz();
+        $this->assertThat($fz->fizzbuzz(15), $this->identicalTo("FizzBuzz"));
+        $this->assertThat($fz->fizzbuzz(30), $this->identicalTo("FizzBuzz"));
+    }
+
+    /**
+     * @test
+     */
+    public function fizzbuzzに5の倍数を渡すとBuzzを返す()
+    {
+        $fz = new Acme\FizzBuzz();
+        $this->assertThat($fz->fizzbuzz(5), $this->identicalTo("Buzz"));
+        $this->assertThat($fz->fizzbuzz(10), $this->identicalTo("Buzz"));
+    }
+
+    /**
+     * @test
+     */
+    public function fizzbuzzに3の倍数を渡すとFizzを返す()
+    {
+        $fz = new Acme\FizzBuzz();
+        $this->assertThat($fz->fizzbuzz(3), $this->identicalTo("Fizz"));
+        $this->assertThat($fz->fizzbuzz(6), $this->identicalTo("Fizz"));
+    }
+
+    /**
+     * @test
+     */
+    public function fizzbuzzに3の倍数でも5の倍数でもない数を渡すとその数の文字列を返す()
+    {
+        $fz = new Acme\FizzBuzz();
+        $this->assertThat($fz->fizzbuzz(1), $this->identicalTo("1"));
+        $this->assertThat($fz->fizzbuzz(2), $this->identicalTo("2"));
     }
 }
