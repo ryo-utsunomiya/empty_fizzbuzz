1cac4d31710f891248b54cb8b71f7543fb924370
Ryo Utsunomiya <utsunomiya@asial.co.jp>
2015-04-22 11:09:05 +0900

    add usage example of assertSame()

--- test/FizzBuzzTest.php
+++ test/FizzBuzzTest.php
@@ -9,6 +9,7 @@ class FizzBuzzTest extends PHPUnit_Framework_TestCase
     */
     public function dummyTest()
     {
-        $this->assertThat(true, $this->isTrue());
+        //$this->assertThat(true, $this->isTrue());
+        $this->assertSame(true, true);
     }
 }
